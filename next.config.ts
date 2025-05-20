1fc7e5150b594462b28d151f28accabe680c8be0
lugondev <lugondev@gmail.com>
2025-05-20 13:54:38 +0700

    update FE: disable ppr

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    ppr: "incremental"
+    ppr: false, // Disable PPR for this app
   },
   images: {
     remotePatterns: [
