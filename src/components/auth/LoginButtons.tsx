3913c2dc8c4993f4aa7408901cab3b127f0403c4
lugondev <lugondev@gmail.com>
2025-07-28 02:11:12 +0700

    feat: Remove Facebook sign-in option from LoginButtons component

--- src/components/auth/LoginButtons.tsx
+++ src/components/auth/LoginButtons.tsx
@@ -3,20 +3,16 @@
 import {useAuth} from '@/contexts/AuthContext'
 import {Button} from '@/components/ui/button'
 // Import icons from lucide-react
-import {Chrome, Facebook, Apple, Twitter} from 'lucide-react' // Added Twitter
+import {Chrome, Apple, Twitter} from 'lucide-react' // Added Twitter
 
 export function LoginButtons() {
-	const {signInWithGoogle, signInWithFacebook, signInWithApple, signInWithTwitter, loading} = useAuth() // Added signInWithTwitter
+	const {signInWithGoogle, signInWithApple, signInWithTwitter, loading} = useAuth() // Added signInWithTwitter
 
 	return (
 		<div className='flex flex-col space-y-2'>
 			<Button onClick={signInWithGoogle} disabled={loading} variant='outline' className='w-full'>
 				<Chrome className='mr-2 h-4 w-4' /> {/* Google Icon */}
 				{loading ? 'Signing in...' : 'Sign in with Google'}
-			</Button>
-			<Button onClick={signInWithFacebook} disabled={loading} variant='outline' className='w-full bg-blue-600 text-white hover:bg-blue-700'>
-				<Facebook className='mr-2 h-4 w-4' /> {/* Facebook Icon */}
-				{loading ? 'Signing in...' : 'Sign in with Facebook'}
 			</Button>
 			<Button onClick={signInWithApple} disabled={loading} variant='outline' className='w-full bg-black text-white hover:bg-gray-800'>
 				<Apple className='mr-2 h-4 w-4' /> {/* Apple Icon */}
